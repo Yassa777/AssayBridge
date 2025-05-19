21a808f6d535ecf341ebd1fd954e22e2ec4e469d
Yasiru <w1926284@my.westminster.ac.uk>
2025-05-19 13:39:49 +0530

    wired appolo into fastify

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,9 +2,12 @@
 import Fastify from 'fastify';
 import ddpcrRoutes from './routes/ddpcr';
 import cors from '@fastify/cors';
+import { ApolloServer } from '@apollo/server';
+import fastifyApollo from '@as-integrations/fastify';
+import { typeDefs } from './graphQL/schema';
+import { resolvers } from './graphQL/resolvers';
 
 const app = Fastify({ logger: true });
-
 // enable CORS
 app.register(cors, { 
   origin: '*',
@@ -13,6 +16,16 @@ app.register(cors, {
 
 // register routes
 app.register(ddpcrRoutes, { prefix: '/api/ddpcr' });
+
+const apollo = new ApolloServer({ typeDefs, resolvers });
+await apollo.start();
+app.register(fastifyApollo(apollo), { prefix: '/graphql' });
+
+await app.listen({ port: 3000 });
+console.log('graphql ready at http://localhost:3000/graphql');
+
+
+
 
 const start = async () => {
   try {
